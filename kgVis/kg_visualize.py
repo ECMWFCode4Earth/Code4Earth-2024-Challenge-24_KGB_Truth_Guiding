e0ba39f549b911263cf90d0163b782e4445b2f9f
Vu Hoang Anh Pham <hoanganhphamvu9698@gmail.com>
2024-06-12 21:38:09 +0000

    Add edge name

--- kgVis/kg_visualize.py
+++ kgVis/kg_visualize.py
@@ -6,41 +6,42 @@ from pyvis.network import Network
 file = "../assets/toy_kg_70b.json"
 
 def get_data_from_file(kg_file) :
-    with open(kg_file, "r") as f :
+    with open(kg_file, 'r') as f :
         data = json.load(f)
-        nodes = data["nodes"]
-        edges = data["relationships"]
+        nodes = data['nodes']
+        edges = data['relationships']
         return (nodes, edges)
 
 def kg_build_kg(nodes, edges, node_color, node_shape, edge_color, cluster_color, cluster_shape) :
-    g = Network(height="1500px", width="100%", bgcolor="#222222", font_color="white")
+    g = Network(height='1500px', width='100%', bgcolor='#222222', font_color='white')
     kg_build_nodes(g, nodes, node_color, node_shape)
     kg_build_edges(g, edges, edge_color, cluster_color, cluster_shape)
     g.barnes_hut(gravity=-5000, damping=1, spring_strength=1)
-    g.show_buttons(filter_="physics")
+    g.show_buttons(filter_='physics')
     g.show("test.html", notebook=False)
 
 def kg_build_nodes(graph, nodes, node_color, node_shape) :
     for node in nodes :
         titre = f"label : {node['label']}\n"
-        for key, value in node["properties"].items() :
+        for key, value in node['properties'].items() :
             titre += f"{key} : {value}\n"
-        name = node["name"]
+        name = node['name']
         graph.add_node(name, color=node_color, shape=node_shape, title=titre)
 
 def kg_build_edges(graph, edges, edge_color, cluster_color, cluster_shape) :
     for edge in edges:
-        start_name = edge["start"]
-        end_name = edge["end"]
+        start_name = edge['start']
+        end_name = edge['end']
         if id_exist_in_kg(graph, start_name) and id_exist_in_kg(graph, end_name) :
-            start = graph.get_node(edge["start"])
-            end = graph.get_node(edge["end"])
-            end["color"] = cluster_color
-            end["shape"] = cluster_shape
+            start = graph.get_node(edge['start'])
+            end = graph.get_node(edge['end'])
+            end['color'] = cluster_color
+            end['shape'] = cluster_shape
             titre = ""
-            for key, value in edge["properties"].items():
+            for key, value in edge['properties'].items():
                 titre += f"{key} : {value}\n"
-            graph.add_edge(start["id"], end["id"], color=edge_color, title=titre)
+            labl = edge['type']
+            graph.add_edge(start['id'], end['id'], color=edge_color, title=titre, label=labl)
 
 def id_exist_in_kg(graph, node_id) :
     return any(node == node_id for node in graph.get_nodes())
